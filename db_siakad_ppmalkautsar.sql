e76ba00f5aed4e24b4e1299dc9c457244ad9a3d4
awinasis <acefmjokam@gmail.com>
2021-05-19 12:16:46 +0700

    db baru

--- db_siakad_ppmalkautsar.sql
+++ db_siakad_ppmalkautsar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 19, 2021 at 06:36 AM
+-- Generation Time: May 19, 2021 at 07:15 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.3.27
 
