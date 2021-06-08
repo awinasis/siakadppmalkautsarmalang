afb31237a20e5b268eaa5b0571a45e68a4b25b8c
awinasis <acefmjokam@gmail.com>
2021-06-08 17:17:02 +0700

    abdullah

--- db_siakad_ppmalkautsar.sql
+++ db_siakad_ppmalkautsar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 08, 2021 at 12:11 PM
+-- Generation Time: Jun 08, 2021 at 12:16 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.3.27
 
